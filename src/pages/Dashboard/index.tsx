b08cef810aea559b43d06108c85c69799aadaabf
Lucca Scarano <lucca.scarano@hotmail.com>
2020-07-22 17:12:33 -0300

    functional page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,16 +2,30 @@ import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
-
 import logoImage from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+
+}
+
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    function handleAddRepository(event: FormEvent<HTMLFormElement>): void{
-        console.log(newRepo);
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
     }
 
     return(
@@ -29,21 +43,22 @@ const Dashboard: React.FC = () => {
             </Form>
 
             <Repositories>
-                <a href="#">
-                    <img
-                        src="https://avatars3.githubusercontent.com/u/48810662?s=400&u=c23598a03d13729c00dad2625da278516495241e&v=4" 
-                        alt="Lucca Scarano"
-                    />
-                    <div>
-                       <strong>Scaralu/Gobarber</strong>
-                       <p>Easy peasy appointments with your preffered barber</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>  
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="#">
+                        <img
+                            src={repository.owner.avatar_url} 
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>  
+                ))}
             </Repositories>
         </>
-    );
+    )
 }
 
 export default Dashboard;
