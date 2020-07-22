553e3dfe0481f81f2ee22596180cc408c3a80fe4
Lucca Scarano <lucca.scarano@hotmail.com>
2020-07-22 18:43:56 -0300

    error handling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
 import logoImage from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
     full_name: string;
@@ -17,15 +17,40 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories);
+        }
+
+        return [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
         event.preventDefault();
 
-        const response = await api.get<Repository>(`repos/${newRepo}`);
-        const repository = response.data;
+        if(!newRepo){
+            setInputError('Digite o autor/nome do repositório')
+            return;
+        }
 
-        setRepositories([...repositories, repository]);
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório')
+        }
     }
 
     return(
@@ -33,7 +58,7 @@ const Dashboard: React.FC = () => {
             <img src={logoImage} alt='Github Explorer' />
             <Title>Explore Repositórios no Github</Title>
 
-            <Form onSubmit={handleAddRepository}>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
                 <input
                     value={newRepo}
                     onChange={(e) => setNewRepo(e.target.value)}
@@ -42,9 +67,11 @@ const Dashboard: React.FC = () => {
                 <button type='submit'>Pesquisar</button>
             </Form>
 
+            { inputError && <Error>{inputError}</Error>}
+
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="#">
+                    <a key={repository.full_name} href="teste">
                         <img
                             src={repository.owner.avatar_url} 
                             alt={repository.owner.login}
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+    hasError: boolean;
+}
 
 // --> Template literals
 
@@ -12,7 +16,7 @@ export const Title = styled.h1`
     color: #3A3A3A;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 715px;
     
@@ -24,6 +28,11 @@ export const Form = styled.form`
         border: 0;
         border-radius: 5px 0 0 5px;
         color: #3A3A3A;
+        border: 2px solid #FFF;
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        `}
 
         &::placeholder {
             color: #A8A8B3;
@@ -48,6 +57,12 @@ export const Form = styled.form`
             background: ${shade(0.2, '#04D361')} 
         }
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
